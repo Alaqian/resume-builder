a97e682d59907a7be2a1201cdafb2742d1f01b93
Alaqian Zafar <a7zafar@gmail.com>
2023-06-03 13:54:18 -0400

    reformatted again

--- resume-builder.py
+++ resume-builder.py
@@ -1,184 +1,215 @@
-import json
+resume_data = {
+    "Personal Information": {
+        "Name": "Jon Doe",
+        "Email": "email@domain.com",
+        "Phone": "(123) 456-7890",
+        "Address": "City, ST",
+        "LinkedIn": "linkedin.com/in/jondoe",
+        "GitHub": "github.com/jondoe"
+    },
+    "Education": {
+        "grad": {
+            "School": "Some University",
+            "Address": "City, ST",
+            "Dates": "September 2021 - Expected May 2023",
+            "Degree": "Master of Science",
+            "Major": "Computer Science",
+            "GPA": "4.0",
+            "Relevant Courses": [
+                "Data Structures and Algorithms",
+                "Copmputer Architecture",
+                "Operating Systems"
+            ]
+        },
+        "undergrad": {
+            "School": "",
+            "Address": "",
+            "Dates": "",
+            "Degree": "",
+            "Major": "",
+            "GPA": "",
+            "Relevant Courses": [
+                "",
+                "",
+                ""
+            ]
+        }
+    },
+    "Work Experience": {
+        "company1": {
+            "Company": "Company 1",
+            "Address": "City, ST",
+            "Dates": "May 2021 - August 2021",
+            "Job Title": "Software Development Intern",
+            "Skills": [
+                "Python",
+                "React",
+                "Django",
+                "PostgreSQL",
+                "Docker",
+                "Jenkins"
+            ],
+            "Bullets": [
+                "Developed a web application using React, Django, and PostgreSQL to display data from a REST API",
+                "Created a Python script to automate the process of generating a report from a database",
+                "Implemented a Docker container to run a Jenkins pipeline to build and deploy the web application"
+            ]
+        },
+        "company2": {
+            "Company": "",
+            "Address": "",
+            "Dates": "",
+            "Job Title": "",
+            "Skills": [
+                "",
+                "",
+                ""
+            ],
+            "Bullets": [
+                "",
+                "",
+                ""
+            ]
+        }
+    },
+    "Skills": {
+        "Programming Languages": [
+            "Javascript",
+            "Python",
+            "Java",
+            "SQL",
+            "C#"
+        ],
+        "Frontend Development": [
+            "React",
+            "Angular",
+            "Vue.js",
+            "jQuery"
+        ],
+        "Backend Development": [
+            "Node.js",
+            "Express.js",
+            "ASP.NET",
+            "Flask",
+            "Django"
+        ],
+        "Databases": [
+            "Oracle",
+            "MySQL",
+            "MongoDB",
+            "PostgreSQL"
+        ],
+        "Tools": [
+            "Git",
+            "Docker",
+            "Jenkins",
+            "Jira",
+            "Confluence"
+        ],
+        "Operating Systems": [
+            "Windows",
+            "Linux",
+            "MacOS"
+        ]
+    },
+    "Projects": {
+        "project1": {
+            "Name": "Resume Builder",
+            "Organization": "some organization",
+            "Dates": "October 2018 - December 2019",
+            "GitHub": "https://github.com/jondoe/test-project",
+            "Bullets": [
+                "Developed a web application using React, Node.js, and MongoDB to generate a resume",
+                "Implemented a REST API to store and retrieve resume data from a database",
+                "Created a Python script to automate the process of generating a report from a database"
+            ],
+            "Skills": [
+                "React",
+                "Node.js",
+                "MongoDB",
+                "Python"
+            ]
+        },
+        "project2": {
+            "Name": "",
+            "Organization": "",
+            "Dates": "",
+            "GitHub": "",
+            "Bullets": [],
+            "Skills": []
+        }
+    }
+}
 
-def generate_resume(json_file, output_file):
-    # Read the JSON file
-    with open(json_file, 'r') as file:
-        data = json.load(file)
-    
-    # Initialize the resume markdown string
-    resume_md = ''
-    
-    # Personal Information section
-    personal_info = data.get('Personal Information')
-    if personal_info:
-        resume_md += '## Personal Information\n\n'
-        for key, value in personal_info.items():
-            resume_md += f"- **{key}:** {value}\n"
-        resume_md += '\n'
-    
-    # Education section
-    education = data.get('Education')
-    if education:
-        resume_md += '## Education\n\n'
-        for key, value in education.items():
-            resume_md += f"### {key.capitalize()}\n"
-            resume_md += f"- **School:** {value['School']}\n"
-            resume_md += f"- **Address:** {value['Address']}\n"
-            resume_md += f"- **Dates:** {value['Dates']}\n"
-            resume_md += f"- **Degree:** {value['Degree']}\n"
-            resume_md += f"- **Major:** {value['Major']}\n"
-            resume_md += f"- **GPA:** {value['GPA']}\n"
-            if value['Relevant Courses']:
-                resume_md += "- **Relevant Courses:** "
-                resume_md += ', '.join(value['Relevant Courses'])
-                resume_md += '\n'
-            resume_md += '\n'
-    
-    # Work Experience section
-    work_experience = data.get('Work Experience')
-    if work_experience:
-        resume_md += '## Work Experience\n\n'
-        for key, value in work_experience.items():
-            resume_md += f"### {value['Job Title']}\n"
-            resume_md += f"- **Company:** {value['Company']}\n"
-            resume_md += f"- **Address:** {value['Address']}\n"
-            resume_md += f"- **Dates:** {value['Dates']}\n"
-            if value['Skills']:
-                resume_md += "- **Skills:** "
-                resume_md += ', '.join(value['Skills'])
-                resume_md += '\n'
-            if value['Bullets']:
-                resume_md += "- **Responsibilities:**\n"
-                for bullet in value['Bullets']:
-                    resume_md += f"  - {bullet}\n"
-            resume_md += '\n'
-    
-    # Skills section
-    skills = data.get('Skills')
-    if skills:
-        resume_md += '## Skills\n\n'
-        for category, category_skills in skills.items():
-            resume_md += f"### {category}\n"
-            resume_md += ', '.join(category_skills)
-            resume_md += '\n\n'
-    
-    # Projects section
-    projects = data.get('Projects')
-    if projects:
-        resume_md += '## Projects\n\n'
-        for key, value in projects.items():
-            resume_md += f"### {value['Name']}\n"
-            resume_md += f"- **Organization:** {value['Organization']}\n"
-            resume_md += f"- **Dates:** {value['Dates']}\n"
-            resume_md += f"- **GitHub:** [{value['GitHub']}]({value['GitHub']})\n"
-            if value['Skills']:
-                resume_md += "- **Skills:** "
-                resume_md += ', '.join(value['Skills'])
-                resume_md += '\n'
-            if value['Bullets']:
-                resume_md += "- **Responsibilities:**\n"
-                for bullet in value['Bullets']:
-                    resume_md += f"  - {bullet}\n"
-            resume_md += '\n'
-    
-    # Save the resume to a file
-    with open(output_file, 'w') as file:
-        file.write(resume_md)
+# Generate a Mardown file from the resume data that is formatted so that it looks like a typical resume
+# Path: resume-builder.py
 
-# Usage example
-json_file = 'resume_data.json'
-output_file = 'resume.md'
-generate_resume(json_file, output_file)
-print(f"Resume saved to {output_file}")
-import json
+def generate_resume(resume_data):
+    # Create a new Markdown file
+    resume_file = open("resume.md", "w")
 
-def generate_resume(json_file, output_file):
-    # Read the JSON file
-    with open(json_file, 'r') as file:
-        data = json.load(file)
-    
-    # Initialize the resume markdown string
-    resume_md = ''
-    
-    # Personal Information section
-    personal_info = data.get('Personal Information')
-    if personal_info:
-        resume_md += '## Personal Information\n\n'
-        for key, value in personal_info.items():
-            resume_md += f"- **{key}:** {value}\n"
-        resume_md += '\n'
-    
-    # Education section
-    education = data.get('Education')
-    if education:
-        resume_md += '## Education\n\n'
-        for key, value in education.items():
-            resume_md += f"### {key.capitalize()}\n"
-            resume_md += f"- **School:** {value['School']}\n"
-            resume_md += f"- **Address:** {value['Address']}\n"
-            resume_md += f"- **Dates:** {value['Dates']}\n"
-            resume_md += f"- **Degree:** {value['Degree']}\n"
-            resume_md += f"- **Major:** {value['Major']}\n"
-            resume_md += f"- **GPA:** {value['GPA']}\n"
-            if value['Relevant Courses']:
-                resume_md += "- **Relevant Courses:** "
-                resume_md += ', '.join(value['Relevant Courses'])
-                resume_md += '\n'
-            resume_md += '\n'
-    
-    # Work Experience section
-    work_experience = data.get('Work Experience')
-    if work_experience:
-        resume_md += '## Work Experience\n\n'
-        for key, value in work_experience.items():
-            resume_md += f"### {value['Job Title']}\n"
-            resume_md += f"- **Company:** {value['Company']}\n"
-            resume_md += f"- **Address:** {value['Address']}\n"
-            resume_md += f"- **Dates:** {value['Dates']}\n"
-            if value['Skills']:
-                resume_md += "- **Skills:** "
-                resume_md += ', '.join(value['Skills'])
-                resume_md += '\n'
-            if value['Bullets']:
-                resume_md += "- **Responsibilities:**\n"
-                for bullet in value['Bullets']:
-                    resume_md += f"  - {bullet}\n"
-            resume_md += '\n'
-    
-    # Skills section
-    skills = data.get('Skills')
-    if skills:
-        resume_md += '## Skills\n\n'
-        for category, category_skills in skills.items():
-            resume_md += f"### {category}\n"
-            resume_md += ', '.join(category_skills)
-            resume_md += '\n\n'
-    
-    # Projects section
-    projects = data.get('Projects')
-    if projects:
-        resume_md += '## Projects\n\n'
-        for key, value in projects.items():
-            resume_md += f"### {value['Name']}\n"
-            resume_md += f"- **Organization:** {value['Organization']}\n"
-            resume_md += f"- **Dates:** {value['Dates']}\n"
-            resume_md += f"- **GitHub:** [{value['GitHub']}]({value['GitHub']})\n"
-            if value['Skills']:
-                resume_md += "- **Skills:** "
-                resume_md += ', '.join(value['Skills'])
-                resume_md += '\n'
-            if value['Bullets']:
-                resume_md += "- **Responsibilities:**\n"
-                for bullet in value['Bullets']:
-                    resume_md += f"  - {bullet}\n"
-            resume_md += '\n'
-    
-    # Save the resume to a file
-    with open(output_file, 'w') as file:
-        file.write(resume_md)
+    # Write the resume header
+    resume_file.write("# " + resume_data["Personal Information"]["Name"] + "\n")
+    resume_file.write(resume_data["Personal Information"]["Email"] + " | " + resume_data["Personal Information"]["Phone"] + " | " + resume_data["Personal Information"]["Address"] + "\n")
+    resume_file.write("[LinkedIn](" + resume_data["Personal Information"]["LinkedIn"] + ") | [GitHub](" + resume_data["Personal Information"]["GitHub"] + ")\n\n")
 
-# Usage example
-json_file = 'resume_data.json'
-output_file = 'resume.md'
-generate_resume(json_file, output_file)
-print(f"Resume saved to {output_file}")
+    # Write the education section
+    resume_file.write("## Education\n\n")
+    resume_file.write("### " + resume_data["Education"]["grad"]["School"] + "\n")
+    resume_file.write(resume_data["Education"]["grad"]["Address"] + " | " + resume_data["Education"]["grad"]["Dates"] + "\n")
+    resume_file.write(resume_data["Education"]["grad"]["Degree"] + " in " + resume_data["Education"]["grad"]["Major"] + " | GPA: " + resume_data["Education"]["grad"]["GPA"] + "\n")
+    resume_file.write("Relevant Courses: " + ", ".join(resume_data["Education"]["grad"]["Relevant Courses"]) + "\n\n")
+
+    resume_file.write("### " + resume_data["Education"]["undergrad"]["School"] + "\n")
+    resume_file.write(resume_data["Education"]["undergrad"]["Address"] + " | " + resume_data["Education"]["undergrad"]["Dates"] + "\n")
+    resume_file.write(resume_data["Education"]["undergrad"]["Degree"] + " in " + resume_data["Education"]["undergrad"]["Major"] + " | GPA: " + resume_data["Education"]["undergrad"]["GPA"] + "\n")
+    resume_file.write("Relevant Courses: " + ", ".join(resume_data["Education"]["undergrad"]["Relevant Courses"]) + "\n\n")
+
+    # Write the work experience section
+    resume_file.write("## Work Experience\n\n")
+    resume_file.write("### " + resume_data["Work Experience"]["company1"]["Company"] + "\n")
+    resume_file.write(resume_data["Work Experience"]["company1"]["Address"] + " | " + resume_data["Work Experience"]["company1"]["Dates"] + "\n")
+    resume_file.write(resume_data["Work Experience"]["company1"]["Job Title"] + "\n")
+    resume_file.write("Skills: " + ", ".join(resume_data["Work Experience"]["company1"]["Skills"]) + "\n")
+    resume_file.write("Bullets:\n")
+    for bullet in resume_data["Work Experience"]["company1"]["Bullets"]:
+        resume_file.write("* " + bullet + "\n")
+    resume_file.write("\n")
+    
+    resume_file.write("### " + resume_data["Work Experience"]["company2"]["Company"] + "\n")
+    resume_file.write(resume_data["Work Experience"]["company2"]["Address"] + " | " + resume_data["Work Experience"]["company2"]["Dates"] + "\n")
+    resume_file.write(resume_data["Work Experience"]["company2"]["Job Title"] + "\n")
+    resume_file.write("Skills: " + ", ".join(resume_data["Work Experience"]["company2"]["Skills"]) + "\n")
+    resume_file.write("Bullets:\n")
+    for bullet in resume_data["Work Experience"]["company2"]["Bullets"]:
+        resume_file.write("* " + bullet + "\n")
+    resume_file.write("\n")
+    
+    # Write the skills section
+    resume_file.write("## Skills\n\n")
+    for skill in resume_data["Skills"]:
+        resume_file.write("### " + skill + "\n")
+        resume_file.write(", ".join(resume_data["Skills"][skill]) + "\n\n")
+        
+    # Write the projects section
+    resume_file.write("## Projects\n\n")
+    resume_file.write("### " + resume_data["Projects"]["project1"]["Name"] + "\n")
+    resume_file.write(resume_data["Projects"]["project1"]["Organization"] + " | " + resume_data["Projects"]["project1"]["Dates"] + "\n")
+    resume_file.write("Skills: " + ", ".join(resume_data["Projects"]["project1"]["Skills"]) + "\n")
+    resume_file.write("Bullets:\n")
+    for bullet in resume_data["Projects"]["project1"]["Bullets"]:
+        resume_file.write("* " + bullet + "\n")
+    resume_file.write("\n")
+    
+    resume_file.write("### " + resume_data["Projects"]["project2"]["Name"] + "\n")
+    resume_file.write(resume_data["Projects"]["project2"]["Organization"] + " | " + resume_data["Projects"]["project2"]["Dates"] + "\n")
+    resume_file.write("Skills: " + ", ".join(resume_data["Projects"]["project2"]["Skills"]) + "\n")
+    resume_file.write("Bullets:\n")
+    for bullet in resume_data["Projects"]["project2"]["Bullets"]:
+        resume_file.write("* " + bullet + "\n")
+    resume_file.write("\n")
+    
+    # Close the file
+    resume_file.close()
+    
+generate_resume(resume_data)
--- resume.md
+++ resume.md
@@ -1,53 +1,38 @@
-## Personal Information
-
-- **Name:** Jon Doe
-- **Email:** email@domain.com
-- **Phone:** (123) 456-7890
-- **Address:** City, ST
-- **LinkedIn:** linkedin.com/in/jondoe
-- **GitHub:** github.com/jondoe
+# Jon Doe
+email@domain.com | (123) 456-7890 | City, ST
+[LinkedIn](linkedin.com/in/jondoe) | [GitHub](github.com/jondoe)
 
 ## Education
 
-### Grad
-- **School:** Some University
-- **Address:** City, ST
-- **Dates:** September 2021 - Expected May 2023
-- **Degree:** Master of Science
-- **Major:** Computer Science
-- **GPA:** 4.0
-- **Relevant Courses:** Data Structures and Algorithms, Copmputer Architecture, Operating Systems
+### Some University
+City, ST | September 2021 - Expected May 2023
+Master of Science in Computer Science | GPA: 4.0
+Relevant Courses: Data Structures and Algorithms, Copmputer Architecture, Operating Systems
 
-### Undergrad
-- **School:** 
-- **Address:** 
-- **Dates:** 
-- **Degree:** 
-- **Major:** 
-- **GPA:** 
-- **Relevant Courses:** , , 
+### 
+ | 
+ in  | GPA: 
+Relevant Courses: , , 
 
 ## Work Experience
 
-### Software Development Intern
-- **Company:** Company 1
-- **Address:** City, ST
-- **Dates:** May 2021 - August 2021
-- **Skills:** Python, React, Django, PostgreSQL, Docker, Jenkins
-- **Responsibilities:**
-  - Developed a web application using React, Django, and PostgreSQL to display data from a REST API
-  - Created a Python script to automate the process of generating a report from a database
-  - Implemented a Docker container to run a Jenkins pipeline to build and deploy the web application
+### Company 1
+City, ST | May 2021 - August 2021
+Software Development Intern
+Skills: Python, React, Django, PostgreSQL, Docker, Jenkins
+Bullets:
+* Developed a web application using React, Django, and PostgreSQL to display data from a REST API
+* Created a Python script to automate the process of generating a report from a database
+* Implemented a Docker container to run a Jenkins pipeline to build and deploy the web application
 
 ### 
-- **Company:** 
-- **Address:** 
-- **Dates:** 
-- **Skills:** , , 
-- **Responsibilities:**
-  - 
-  - 
-  - 
+ | 
+
+Skills: , , 
+Bullets:
+* 
+* 
+* 
 
 ## Skills
 
@@ -72,17 +57,15 @@ Windows, Linux, MacOS
 ## Projects
 
 ### Resume Builder
-- **Organization:** some organization
-- **Dates:** October 2018 - December 2019
-- **GitHub:** [https://github.com/jondoe/test-project](https://github.com/jondoe/test-project)
-- **Skills:** React, Node.js, MongoDB, Python
-- **Responsibilities:**
-  - Developed a web application using React, Node.js, and MongoDB to generate a resume
-  - Implemented a REST API to store and retrieve resume data from a database
-  - Created a Python script to automate the process of generating a report from a database
+some organization | October 2018 - December 2019
+Skills: React, Node.js, MongoDB, Python
+Bullets:
+* Developed a web application using React, Node.js, and MongoDB to generate a resume
+* Implemented a REST API to store and retrieve resume data from a database
+* Created a Python script to automate the process of generating a report from a database
 
 ### 
-- **Organization:** 
-- **Dates:** 
-- **GitHub:** []()
+ | 
+Skills: 
+Bullets:
 
